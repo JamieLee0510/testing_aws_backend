1277c0a3f349f3797fd726c5ea346fd1d93961dc
ding <walker200665@gmail.com>
2021-03-18 22:59:41 +0800

    update port

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const cors = require("cors");
 
 //inorder to read .env
-require("dotenv").config();
+// require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.port || 3000;
 
 app.use(cors());
 app.use(express.json());
